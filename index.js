3f20af74a485e62e6547a3851e3a18284cf16204
Michael Harlow <mharlow@mycod.us>
2024-04-18 16:40:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let saveEl = document.getElementById("save-el")
+let entriesEl = document.getElementById("entries-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
@@ -10,8 +10,9 @@ function increment() {
 function save() {
     let currentDate = new Date();
     let currentTime = currentDate.getHours() + ":" + (currentDate.getMinutes() < 10 ? '0' : '') + currentDate.getMinutes() + ":" + (currentDate.getSeconds() < 10 ? '0' : '') + currentDate.getSeconds();
-    let countStr = count + " ( " + currentTime + " ) - "
-    saveEl.textContent += countStr
+    let countStr = document.createElement("li")
+    countStr.textContent = count + " ( " + currentTime + " )"
+    entriesEl.appendChild(countStr)
     countEl.textContent = 0
     count = 0
 }
